5821744853fdcc3e7d4e566cd526ab0c25b9fbb8
Harry Alvarado <harry2alvarado8@gmail.com>
2018-07-31 23:31:32 -0500

    Limpiando viejo código y actualizando repositorio.

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: harry
+ * Date: 07/31/18
+ * Time: 1:25 PM
+ */
+
+$var0 = 28;
+$var1 = 21;
+
+echo "Hello World!, ¿Que tal?<br>";
+
+if ($var0 > $var1){
+    echo $var0," es mayor que ",$var1;
+}else{
+    echo $var1," es mayor que ",$var0;
+}
