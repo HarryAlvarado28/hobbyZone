0e73d448aa549393e837043c93f7460233d4e772
Harry Alvarado <harry2alvarado8@gmail.com>
2018-08-03 14:13:55 -0500

    Se agregan funciones y libreria para generar Codigo de Barra e imprimir. Sección:9, Clase 37.

--- DWPHP1/vistas/articulo.php
+++ DWPHP1/vistas/articulo.php
@@ -76,7 +76,6 @@ require 'header.php';
                           </div>
                           <div class="form-group col-lg-12 col-md-12 col-sm-12 col-xs-12">
                             <button class="btn btn-primary" type="submit" id="btnGuardar"><i class="fa fa-save"></i> Guardar</button>
-
                             <button class="btn btn-danger" onclick="cancelarform()" type="button"><i class="fa fa-arrow-circle-left"></i> Cancelar</button>
                           </div>
                         </form>
