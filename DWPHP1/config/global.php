6641c78f9b53807ed1e9e56bc6aabd79a6e8002e
Harry Alvarado <harry2alvarado8@gmail.com>
2018-08-02 22:20:46 -0500

    Organización de la plantilla, Sección:7, Clase 19.

---
+++ DWPHP1/config/global.php
@@ -0,0 +1,19 @@
+<?php 
+//Ip de la pc servidor de base de datos
+define("DB_HOST","192.168.0.9");
+
+//Nombre de la base de datos
+define("DB_NAME", "dwphp1");
+
+//Usuario de la base de datos
+define("DB_USERNAME", "dwphp1user");
+
+//Contraseña del usuario de la base de datos
+define("DB_PASSWORD", "HBAU1428");
+
+//definimos la codificación de los caracteres
+define("DB_ENCODE","utf8");
+
+//Definimos una constante como nombre del proyecto
+define("PRO_NOMBRE","ITVentas");
+?>
