6fa217fb3d42854257b20ada8da08fcdb4d151cd
Harry Alvarado <harry2alvarado8@gmail.com>
2018-08-03 00:29:55 -0500

    Se continua con el desarrollo del proyecto tranbajando esta ves en el frontend, Sección:9, Clase 24.

--- DWPHP1/vistas/footer.php
+++ DWPHP1/vistas/footer.php
@@ -11,5 +11,16 @@
     <script src="../public/js/bootstrap.min.js"></script>
     <!-- AdminLTE App -->
     <script src="../public/js/app.min.js"></script>
+
+    <!-- DATATABLES -->
+    <script src="../public/datatables/jquery.dataTables.min.js"/>
+    <script src="../public/datatables/dataTables.buttons.min.js"/>
+    <script src="../public/datatables/buttons.html5.min.js"/>
+    <script src="../public/datatables/buttons.colVis.min.js"/>
+    <script src="../public/datatables/jszip.min.js"/>
+    <script src="../public/datatables/pdfmake.min.js"/>
+    <script src="../public/datatables/vfs_fonts.js"/>
+    <script src="../public/js/bootstrap.min.js"/>
+
   </body>
 </html>
