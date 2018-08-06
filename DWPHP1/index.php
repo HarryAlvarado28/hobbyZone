afaf1d38f123f27b29d7c1b3ec86e75e50df9e4b
Harry Alvarado <harry2alvarado8@gmail.com>
2018-08-06 14:07:25 -0500

    Acceso al sistema, menu de login. Sección 10, Clase 53.

--- DWPHP1/index.php
+++ DWPHP1/index.php
@@ -6,7 +6,7 @@
  * Time: 1:25 PM
  */
 
-$var0 = 28;
+/*$var0 = 28;
 $var1 = 21;
 
 echo "Hello World!, ¿Que tal?<br>";
@@ -15,4 +15,7 @@ if ($var0 > $var1){
     echo $var0," es mayor que ",$var1;
 }else{
     echo $var1," es mayor que ",$var0;
-}
+}*/
+
+header('Location: vistas/login.html');
+?>
