67258d6cb6b5fa08e605c2c6705759695b2678f2
harryalvarado <harry2alvarado8@gmail.com>
2018-08-30 09:11:10 -0500

    Service - Se crea un modulo de servicio el cual nos ayudara ha brindar los servicio que necesite los componentes de nuestra aplicación.

--- Angular/v02-spa/src/app/app.module.ts
+++ Angular/v02-spa/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { NgModule } from '@angular/core';
 import { APP_ROUTING } from './app.routes';
 
 // Servicios
-
+import {HeroesService} from './servicios/heroes.service';
 // Componentes
 import { AppComponent } from './app.component';
 import { SharedComponent } from './components/shared/shared.component';
@@ -28,7 +28,9 @@ import { HeroesComponent } from './components/heroes/heroes.component';
     BrowserModule,
     APP_ROUTING
   ],
-  providers: [],
+  providers: [
+    HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- Angular/v02-spa/src/app/components/heroes/heroes.component.ts
+++ Angular/v02-spa/src/app/components/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService, Heroe } from '../../servicios/heroes.service';
 
 @Component({
   selector: 'app-heroes',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  heroes:Heroe[] = [];
+
+  constructor(private _heroesService:HeroesService) {
+    console.log("Constructor");
+  }
 
   ngOnInit() {
+    this.heroes = this._heroesService.getHeroes();
+    console.log (this.heroes);
   }
 
 }
