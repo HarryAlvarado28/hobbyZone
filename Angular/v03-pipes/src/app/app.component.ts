ab32762188ca21bec145f6d3b1898247c0742a9b
harryalvarado <harry2alvarado8@gmail.com>
2018-09-03 00:00:02 -0500

    El tema tratado en este mini-proyecto practico es los PIPES.

--- Angular/v03-pipes/src/app/app.component.ts
+++ Angular/v03-pipes/src/app/app.component.ts
@@ -7,5 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipes';
-  nombre = 'Harry'
+  nombre = 'Harry';
+  nombreCompleto = 'harry b AlVArado U';
+  arreglo = [1,2,3,4,5,6,7,8,9,10]
+  PI = Math.PI;
+  a = 0.285;
+  salario = 12345.6;
+  heroe = {
+    nombre: "Clar Ken",
+    clase: "Superman",
+    edad: 34,
+    dirección: {
+      calle: "Primera",
+      casa: 14
+    }
+  };
+  valorDePromesa = new Promise((resolve,reject)=>{
+    setTimeout(()=>resolve('llego la data'),3600);
+  })
+  fecha = new Date();
+  video = 'IvTio0FJf6Y';
+  activar = true;
 }
--- Angular/v03-pipes/src/app/app.module.ts
+++ Angular/v03-pipes/src/app/app.module.ts
@@ -2,15 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CapitalizadoPipe } from './pipes/capitalizado.pipe';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
+import { ContrasenaPipe } from './pipes/contrasena.pipe';
+
+/*
+  Los ultimos paquetes fueron agregado para cambiar el
+  idioma, en español.
+*/
+import localeEs from '@angular/common/locales/es';
+import {registerLocaleData} from '@angular/common';
+import {LOCALE_ID} from '@angular/core';
+
+registerLocaleData(localeEs);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapitalizadoPipe,
+    DomseguroPipe,
+    ContrasenaPipe
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [{provide: LOCALE_ID, useValue:"es"}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
