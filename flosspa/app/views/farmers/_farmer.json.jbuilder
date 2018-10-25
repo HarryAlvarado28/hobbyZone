01cbb1a0bd4893694296ca1f9a96425b6a478c88
harryalvarado <harry2alvarado8@gmail.com>
2018-10-24 23:24:32 -0500

    Se remueve archivos de más.

--- flosspa/app/views/farmers/_farmer.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! farmer, :id, :name, :email, :phone_number, :image, :created_at, :updated_at
-json.url farmer_url(farmer, format: :json)
--- flosspa/config/routes.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.routes.draw do
-  resources :farmers
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
