f340dd8bd2d68b30847f984915c4c834459d0cb4
harryalvarado <harry2alvarado8@gmail.com>
2018-10-28 23:42:42 -0500

    Manejo de formulario con DATA, seteando y reseteando.

--- Angular/v08-formularios/src/app/components/data/data.component.ts
+++ Angular/v08-formularios/src/app/components/data/data.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import {validate} from 'codelyzer/walkerFactory/walkerFn';
 
 @Component({
   selector: 'app-data',
@@ -10,18 +11,62 @@ export class DataComponent {
 
   forma: FormGroup;
 
+  usuario: any = {
+    nombrecompleto: {
+      nombre: "Harry",
+      apellido: "Alvarado"
+    },
+    correo: "harry.alvarado@up.ac.pa"
+  }
+
+
   constructor() {
+
+    console.log(this.usuario);
+
     this.forma = new FormGroup({
-      'nombre': new FormControl('Harry'),
-      'apellido': new FormControl(),
-      'correo': new FormControl()
+
+      'nombrecompleto': new FormGroup({
+          'nombre': new FormControl(
+            '',
+            [
+              Validators.required,
+              Validators.minLength(3)
+            ]),
+          'apellido': new FormControl(
+            '',
+            Validators.required)
+        }),
+      'correo': new FormControl('',
+              [
+                Validators.required,
+                Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}$")
+              ])
     })
+
+    // setear valores en el formulario
+      this.forma.setValue(this.usuario);
 
   }
 
   guardarCambios(){
     console.log(this.forma.value);
     console.log(this.forma);
+
+    /*
+      forma nº1 de reseterar valores
+        this.forma.reset(this.usuario);
+    */
+    /*
+      forma nº2 de reseterar valores, individualmente.
+    */
+        this.forma.reset({
+          nombrecompleto: {
+            nombre: "",
+            apellido: ""
+          }, correo: ""
+        });
+
   }
 
 }
